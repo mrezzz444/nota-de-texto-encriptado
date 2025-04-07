59e5479359b9c2852d46bf4d9beabdea922e37be
IsaacAvila29 <isaac.avila.saenz29@gmail.com>
2025-04-07 12:29:27 -0600

    refactor: remove old example file and update backend logic for note saving

--- elbackend.php
+++ elbackend.php
@@ -1,18 +1,17 @@
-<html language="sp-mx">
-    <head><title>  ejemplo </title></head>
+<html lang="es-MX">
+    <head><title>Ejemplo</title></head>
     <body>
         <?php
-        //ESTE DOCUMENTO TOCA PEGARLO DENTRO DEL XAMPP
-         if(isset($_POST['Encriptar']))
-         {
-            // TO DO Hacer el filename personalizable
-            $ar=fopen("datosgb.txt","a")or die("problemas de acceso");
-            fputs($ar,$_REQUEST['nota']);
-            fputs($ar,"\n");
-
-            echo "<br> <b> SE GUARDO CORRECTAMENTE : </b><br><br>";
-
-         }
+        // ESTE DOCUMENTO TOCA PEGARLO DENTRO DEL XAMPP
+        if (isset($_POST['nota'])) {
+            // TO DO: Hacer el filename personalizable
+            $ar = fopen("Losdatosgb.txt", "a") or die("Problemas de acceso");
+            fputs($ar, $_POST['nota']);
+            fputs($ar, "\n");
+            echo "<br><b>SE GUARDÓ CORRECTAMENTE:</b><br><br>";
+        } else {
+            echo "<br><b>Error: No se recibió ninguna nota.</b><br><br>";
+        }
         ?>
     </body>
-</html> 
+</html>
