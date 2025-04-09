9d36b082b24b9937ba992fb8c123432608d66010
IsaacAvila29 <isaac.avila.saenz29@gmail.com>
2025-04-09 12:13:08 -0600

    fix: refactor note saving logic and improve file reading functionality

--- elbackend.php
+++ elbackend.php
@@ -6,6 +6,7 @@
         error_reporting(E_ALL);
         ini_set('display_errors', 1);
 
+        // Guardar nota
         if (isset($_POST['nota'])) {
             $filename = "Losdatosgb.txt";
 
@@ -16,23 +17,29 @@
             }
 
             $TextoOriginal = $_POST['nota'];
-            fputs($ar, $_POST['nota'] . "\n");
+            fputs($ar, $TextoOriginal . "\n");
             fclose($ar);
 
             echo "<br><b>SE GUARDÓ CORRECTAMENTE:</b><br><br>";
+        } elseif (isset($_POST['leer'])) {
+            $filename = "Losdatosgb.txt";
+
+            // Verificar si el archivo existe
+            if (file_exists($filename)) {
+                $ar = fopen($filename, "r") or die("<b>Error:</b> No se pudo abrir el archivo.");
+
+                // Leer y mostrar el contenido del archivo
+                while (!feof($ar)) {
+                    $vTexto = fgets($ar);
+                    $nuevoTexto = nl2br($vTexto);
+                    echo $nuevoTexto;
+                }
+                fclose($ar);
+            } else {
+                echo "<br><b>Error:</b> El archivo no existe.</b><br><br>";
+            }
         } else {
-            echo "<br><b>Error: No se recibió ninguna nota.</b><br><br>";
-        }
-        elseif (isset($_POST('leer'))){
-           if(file_exists($ar));
-           $ar = fopen("datosgb.txt","r")or die("Problemas, problemas");
-            
-         while(!feof($ar)){
-            $vTexto = fgets($ar);
-            $nuevoTexto = nl2br($vTexto);
-            echo $nuevoTexto;
-         }
-         fclose($ar);
+            echo "<br><b>Error:</b> No se recibió ninguna acción válida.</b><br><br>";
         }
         ?>
     </body>
